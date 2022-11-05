d27aab9327b039ce464a5c4112a709e85fa3f9a5
vladzhuganov <vladzhuganov@gmail.com>
2022-11-04 21:19:21 -0400

    third commit

--- src/TestApp.java
+++ src/TestApp.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class TestApp {
-// comment from remote
-    Scanner scanner = new Scanner(System.in);
+    // comment from remote
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please, enter a string");
         String str = scanner.nextLine();
         int vCount = 0, cCount = 0;
@@ -8,12 +13,12 @@ public class TestApp {
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
                 vCount++;
-            } else if (str.charAt(i) >= 'a' && str.charAt(i)<='z') {
+            } else if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
                 cCount++;
             }
         }
         System.out.println("Total Vowels - " + vCount + "," + "Total Consonants -  " + cCount);
     }
-
+}
 
 }
